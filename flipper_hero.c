1cc7e90198c280239b694d6cfaea8e820bd0873e
Lucas Oliveira <lucaspwo@gmail.com>
2024-08-28 16:35:16 -0300

    changed to conform with API 72.1

--- flipper_hero.c
+++ flipper_hero.c
@@ -46,8 +46,9 @@ void end_game(PluginState* plugin_state) {
     plugin_state->isGameOver = true;
 }
 
-static void input_callback(InputEvent* input_event, FuriMessageQueue* event_queue) {
-    furi_assert(event_queue);
+static void input_callback(InputEvent* input_event, void* ctx) {
+
+    FuriMessageQueue* event_queue = ctx;
     PluginEvent event = {.type = EventTypeKey, .input = *input_event};
     furi_message_queue_put(event_queue, &event, FuriWaitForever);
 }
