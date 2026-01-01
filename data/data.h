e592ed2fe6fdb5a0cdd593d218359a5843a17ec6
bdwph6 <beckettwag@gmail.com>
2025-12-31 23:09:35 -0600

    ohgh,,,

--- data/data.h
+++ data/data.h
@@ -1,4 +1,5 @@
 #include <input/input.h>
+#include <gui/icon.h>
 #pragma once
 typedef enum {
     EventTypeTick,
@@ -16,17 +17,25 @@ typedef struct {
     bool arrowFilled[8]; // Track whether each arrow is filled
     int numArrows; // Number of arrows currently active
     int nextArrowToFill; // Index of the next arrow to fill
-    char stratagems[81][8]; // Stratagems. 78 main stratagems + hellbomb and reinforce mission stratagems.
-                            // From https://helldivers.wiki.gg/wiki/Stratagems
-    int stratagem_queue[6];
+    int queue_spot;
+    char name_buf[32];
+    char seq_buf[9];
+    int stratagem_queue[20];
     int score;
     int round;
     int timer;
     int32_t record_score;
     int32_t record_round;
+    int bonuses[3]; // round bonus at 0, time bonus at 1, perfect bonus at 2.
     bool isGameStarted;
+    bool isScoreScreen;
+    bool isPreRound;
+    bool isRoundOver;
+    bool perfect;
     bool isGameOver;
 } PluginState;
+
+
 
 typedef enum {
     ArrowDirectionUp,
